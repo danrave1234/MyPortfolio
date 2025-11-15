57d82daddbe50c40111c5e11cdf2ebdbe783e6dc
danrave1234 <danravekeh123@gmail.com>
2025-11-16 07:32:56 +0800

    Replace profile picture: Update from mee.jpg to Me.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import me from './assets/mee.jpg';
+import me from './assets/Me.png';
 import Header from './components/Header';
 import About from './components/About';
 import Experience from './components/Experience';
